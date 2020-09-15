1aaab2b39de98ff7ac8c97d0261eee9c1e763038
Hakim Laoukili <hakimlaoukili@gmail.com>
2020-09-15 23:33:06 +0200

    Update app.js
    Greated postman api and adde to firebase

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 var firebaseConfig = {
     apiKey: "AIzaSyD8X5-tO3DvwWq9iEFyAWPZGj_z6WSXS1M",
     authDomain: "massageform-86827.firebaseapp.com",
@@ -12,8 +14,10 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
-let messagesRef = firebase.database().ref('messages').set;
+
+let messagesRef = db.doc('messages');
 
 
 /* skicka funtionen */
@@ -51,19 +55,42 @@ function getInputVal(id) {
 /* Spara meddelande till firebase */
 
 function saveMassage(name, company, email, phone, message) {
+    try {
 
-    let newMassageRef = massagesRef.push();
-    /* Sätter in datta i objetet till firebase */
-    newMassageRef.set({
-        name: name,
-        company: company,
-        email: email,
-        phone: phone,
-        message: message
+        /* let newMassageRef = messagesRef.push(); */
+        /* Sätter in datta i objetet till firebase */
+        messagesRef.set({
+            name: name,
+            company: company,
+            email: email,
+            phone: phone,
+            message: message
 
 
 
-    });
+        });
+
+
+    } catch (error) {
+        console.error(error);
+
+    }
+
+    function readFirebase() {
+
+        messagesRef.get().then(function (doc) {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function (error) {
+            console.log("Error getting document:", error);
+        });
+
+
+    }
 
 
 }
